cfe8fdc09e6e3d939a6cc3d457d95f81ed05ec0d
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-08-06 00:08:59 +0300

    built diagnoses api

--- clinical/views.py
+++ clinical/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin
+from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin,ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 from clinical.models import Diagnosis
@@ -8,7 +8,7 @@ from clinical.serializers import DiagnosisSerializer
 from users.permissions import IsDoctorPermission
 # Create your views here.
 
-class DiagnosesViewSet(CreateModelMixin,RetrieveModelMixin, GenericViewSet):
+class DiagnosesViewSet(CreateModelMixin,RetrieveModelMixin, ListModelMixin,GenericViewSet):
     queryset = Diagnosis.objects.all()
     serializer_class = DiagnosisSerializer
     permission_classes = [IsDoctorPermission]
