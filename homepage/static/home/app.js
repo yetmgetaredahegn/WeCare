ea6e1ad2b84e87aa6b96b1406511217bc2ef84c4
Yetmgeta Redahegn <yetmgeta.tech@gmail.com>
2025-08-25 01:00:47 +0300

    easy to navigate endpoints

---
+++ homepage/static/home/app.js
@@ -0,0 +1,7 @@
+const usersLink = document.getElementById("usersLink")
+
+usersLink.addEventListener("click", function(event){
+    event.preventDefault();
+    fetch("/users/")
+        .then(response => response.json())
+})
